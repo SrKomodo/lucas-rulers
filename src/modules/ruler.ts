426e2ee599df6ec37976978af7ae68794dff6574
SrKomodo <komodosr@gmail.com>
2018-01-11 01:35:33 -0300

    Implement triangle generation

--- src/modules/ruler.ts
+++ src/modules/ruler.ts
@@ -1,16 +1,46 @@
 class Ruler {
   protected numbers: number[][];
+  protected triangles: number[][]; // [start, end, result]
 
   constructor(digit: number) {
     this.numbers = [];
-    for (let i = 1; i <= 9; i++) {
-      const nums = [];
-      for (let j = 0; j < i; j++) {
-        nums.push((i * digit + j) % 10);
+    this.triangles = [];
+
+    for (let i = 1; i <= 9; i++) {     // Iterate over each row in the ruler
+      const row = [];
+      const rowTriangles = [];
+
+      let num = digit * i;             // First number in row
+      let lastCarry;                   // Name is self-explanatory
+      let trigI = 0;                   // Current triangle index (Could be removed)
+      rowTriangles.push([0]);          // Add starting triangle vertex
+
+      for (let j = 0; j < i; j++) {    // For each number in the row
+        if (num >= 10) {                                 // If we carry 2 digits
+          const carry = parseInt(num.toString()[0], 10); // Get the carried digit
+          if (typeof lastCarry === "undefined") {                              // If this is the first carry
+            lastCarry = carry;                           // Set this carry as first
+          } else if (carry !== lastCarry) {              // If we are at a new carry
+            rowTriangles[trigI].push(j - 1, lastCarry);  // Close the current triangle
+            rowTriangles.push([j]); trigI++;             // Create a new one
+            lastCarry = carry;                           // And set the new carry
+          }
+        } else if (j === 0) { // If there was no carry on the first number
+          lastCarry = 0;      // Then we set it to 0
+        }
+        row.push(num % 10); // Add number remainder to row
+        num++;              // And bump up the number
       }
-      this.numbers.push(nums);
+
+      rowTriangles[trigI].push(i - 1, lastCarry); // Close last triangle
+
+      this.triangles.push(rowTriangles);
+      this.numbers.push(row);
     }
     console.log(this.numbers);
+    console.log(this.triangles.map((value) => {
+      return value.map((n) => `${n[2]} ${n[0]}\n  ${n[1]}`).join("\n");
+    }).join("\n===\n"));
   }
 }
 
